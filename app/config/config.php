0d13525fe15657c73192d54573a64b485dd4dc77
Anbu Venkat <anbu25@gmail.com>
2018-08-16 22:03:25 -0700

    Updated the config.php with generic setup

--- app/config/config.php
+++ app/config/config.php
@@ -2,6 +2,6 @@
     //declare app root
     define('APPROOT', dirname(dirname(__FILE__)));
     //declare url root
-    define('URLROOT', 'https://localhost/shareposts/');
+    define('URLROOT', '_YOUR_URL_ROOT_');
     //declare site name
-    define('SITENAME', 'Share Posts');
+    define('SITENAME', '_YOUR_SITE_NAME');
